6eabbf1e1e9f6350f51a2abcbe2debcaa3a9582b
blueddie <blueddie@naver.com>
2024-03-29 14:14:56 +0900

    bce

---
+++ tf114/tf16_00_iris.py
@@ -0,0 +1,56 @@
+from sklearn.datasets import load_iris
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error,accuracy_score
+
+# 1. 데이터
+datasets = load_iris()
+
+x , y = datasets.data, datasets.target
+
+x = x[ y != 2]
+y = y[ y != 2]
+
+# print(x.shape, y.shape)   # (100, 4) (100,)
+
+y = np.reshape(y, [-1, 1])
+# print(x.shape, y.shape)   # (100, 4) (100, 1)
+
+xp = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])  # x placeholder
+yp = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])  # y placeholder
+
+w = tf.compat.v1.Variable(tf.compat.v1.random_normal([4, 1]), dtype=tf.float32, name='weights')
+# w = tf.compat.v1.Variable(tf.compat.v1.random_normal([2, 1]), dtype=tf.float32, name='weights')
+
+b = tf.compat.v1.Variable(tf.compat.v1.zeros([1]), dtype=tf.float32)
+# b = tf.compat.v1.Variable(tf.compat.v1.zeros([1]), dtype=tf.float32)
+
+
+# 2. 모델
+hypothesis = tf.compat.v1.sigmoid(tf.compat.v1.matmul(xp, w) + b)
+
+
+# 3-1. 컴파일
+loss = -tf.reduce_mean(yp * tf.log(hypothesis) + (1 - yp) * tf.log(1 - hypothesis))   # 'binary_crossentropy'
+
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=1e-1)
+
+train = optimizer.minimize(loss)
+
+# 3-2. 훈련
+with tf.compat.v1.Session() as sess:
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    epochs = 1000
+
+    for step in range(epochs):
+        _, loss_val, w_val, b_val = sess.run([train, loss, w, b], feed_dict={xp:x, yp:y})
+        if (step + 1) % 20:
+            print(step + 1, "\t", "loss : ", loss_val)
+
+    x_test = tf.compat.v1.placeholder(tf.float32, shape=[None,4])
+    y_pred = tf.sigmoid(tf.matmul(x_test, w_val) + b_val)
+
+    predict = sess.run(tf.cast(y_pred > 0.5 , dtype=tf.float32), feed_dict={x_test : x})
+    acc = accuracy_score(y, predict)
+    print("acc : ", acc)
