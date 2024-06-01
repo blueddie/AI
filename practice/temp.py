007a3ef3fd82650a20b10c62b8f116ec1f086dc7
blueddie <blueddie@naver.com>
2024-06-01 13:05:58 +0900

    Update temp.py

--- practice/temp.py
+++ practice/temp.py
@@ -1,39 +1,37 @@
-import cv2  # OpenCV 라이브러리 임포트
-import os   # 파일 시스템 관리를 위한 os 모듈 임포트
+import numpy as np
 
-def extract_frames(video_path, output_folder):
-    """
-    영상 파일에서 프레임을 추출하여 이미지 파일로 저장하는 함수
-    :param video_path: 추출할 영상 파일 경로
-    :param output_folder: 추출된 프레임을 저장할 폴더 경로
-    """
-    # 영상 파일 열기
-    video_capture = cv2.VideoCapture(video_path)
-    # 프레임 카운트 초기화
-    frame_count = 0
+aaa = np.array([-10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 50])
 
-    # 영상이 열렸는지 확인
-    while video_capture.isOpened():
-        # 프레임 읽기
-        ret, frame = video_capture.read()
-        if not ret:  # ret이 False면 영상이 끝난 것이므로 루프를 종료
-            break
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out, [25, 50, 75])
+    
+    print("1사분위 : ", quartile_1)
+    print("q2 : ", q2)
+    print("3사분위 : ", quartile_3)
+    
+    iqr = quartile_3 - quartile_1
+    print("iqr : ", iqr)
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    
+    return np.where((data_out>upper_bound) |
+                    (data_out<lower_bound))
+    
+outliers_loc = outliers(aaa)
+print("이상치의 위치 : ", outliers_loc)
 
-        # 프레임 저장
-        frame_count += 1
-        frame_name = f"frame_{frame_count}.jpg"  # 프레임 파일명 생성
-        frame_path = os.path.join(output_folder, frame_name)  # 프레임 저장 경로 생성
-        # cv2.imwrite(frame_path, frame)  # 프레임을 이미지 파일로 저장
-        cv2.imwrite(frame_path, frame)  # 프레임을 이미지 파일로 저장
+import matplotlib.pyplot as plt
+plt.boxplot(aaa)
+plt.show()
 
-    # 영상 파일 닫기
-    video_capture.release()
-    cv2.destroyAllWindows()
+# 주어진 데이터에서 사분위수(quantile)를 계산합니다. 이를 위해 np.percentile() 함수를 사용하여 데이터의 25%, 50%, 75%에 해당하는 사분위수를 구합니다. 이때 각각 1사분위수(quartile_1), 중앙값(q2), 3사분위수(quartile_3)로 할당됩니다.
 
-# 영상 파일 경로 설정
-video_path = 'C:\\group_project_data\\train\\123.mp4'
-# 프레임을 저장할 폴더 설정
-output_folder = 'C:\\group_project_data\\test'
+# 사분위수를 기반으로 사분위범위(IQR, Interquartile Range)를 계산합니다. IQR은 3사분위수에서 1사분위수를 뺀 값으로, 데이터의 중간 50% 범위를 나타냅니다.
 
-# 프레임 추출 실행
-extract_frames(video_path, output_folder)
+# 이상치의 하한(lower bound)과 상한(upper bound)을 계산합니다. 일반적으로 하한은 1사분위수에서 1.5배의 IQR을 뺀 값이고, 상한은 3사분위수에 1.5배의 IQR을 더한 값입니다.
+
+# 수정된 부분에서는 상한(upper bound)을 quartile_3에 1.5배의 IQR을 더한 값으로 계산합니다.
+
+# 이상치의 위치를 찾습니다. 이상치는 하한보다 작거나 상한보다 큰 데이터입니다. np.where() 함수를 사용하여 해당 조건을 만족하는 데이터의 위치를 찾습니다.
+
+# 이상치의 위치를 반환합니다.
